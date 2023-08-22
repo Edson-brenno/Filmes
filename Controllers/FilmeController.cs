a15a8e08caffb0d5669cf96735f3bcf2885743e1
brenno <brennorebeiro@gmail.com>
2023-08-21 21:35:54 -0300

    Estudo dia 21/08/2023

--- Controllers/FilmeController.cs
+++ Controllers/FilmeController.cs
@@ -11,15 +11,29 @@ namespace FilmesApi.Controllers{
         private static List<Filme> filmes = new List<Filme>();
         // Adiciona filme
         [HttpPost]
-        public void AdicionaFilme([FromBody] Filme filmeNovo){
+        public IActionResult AdicionaFilme([FromBody] Filme filmeNovo){
             filmes.Add(filmeNovo);
-            System.Console.WriteLine("Escreveu");
+            return CreatedAtAction(nameof(GetFilmeId), new {id = filmeNovo.id},filmeNovo);
         }
 
         //Obter os filmes
         [HttpGet]
-        public string GetFilmes(){
-            return "Seila";
+        public IEnumerable<Filme> GetFilmes([FromQuery] int pule = 0, [FromQuery] int pegue = 3){
+            return filmes.Skip(pule).Take(pegue);
+        }
+
+        //Obter um filme:
+        [HttpGet("id/{id}")]
+        public IActionResult GetFilmeId(int id){
+            Filme filme= filmes.FirstOrDefault(x => x.id == id);
+            if (filme == null){ return NotFound();};
+            return Ok(filme);
+        }
+
+        [HttpGet("nome/{nome}")]
+
+        public List<Filme>? GetFilmeByName(string nome){
+            return filmes.FindAll(x => x.titulo.StartsWith(nome.ToUpper()));
         }
     }
 }
--- Model/FilmesModel.cs
+++ Model/FilmesModel.cs
@@ -2,12 +2,24 @@ using System.ComponentModel.DataAnnotations;
 
 namespace FilmesApi.Models{
     public class Filme{
+        
+        public static int numberid = 0; 
+        public int id {get; private set;}
+
         [Required(ErrorMessage = "O titulo do filme não pode ser nulo")]
         [StringLength(500, ErrorMessage = "Titulo tem que ser menor que 500 caracteres")]
-        public string titulo {get; set;}
+        private string _titulo;
+        public string titulo {
+            get { return _titulo; }
+            set { _titulo = value.ToUpper(); }
+        }
 
         [Required(ErrorMessage = "O genero não pode ser nulo")]
-        public string genero {get; set;}
+        private string _genero;
+        public string genero {
+            get {return _genero; }
+            set { _genero = value.ToUpper();}
+        }
 
         [Required(ErrorMessage = "O ano não pode ser nulo")]
         [Range(1900,2023, ErrorMessage = "O ano do filme deve estar entre 1900 à 2023")]
@@ -20,6 +32,10 @@ namespace FilmesApi.Models{
 
         override public string ToString(){
             return $"{titulo} \n {genero} \n {ano} \n {duracao}";
+        }
+
+        public Filme(){
+            this.id = numberid++;
         }
     }
 }
